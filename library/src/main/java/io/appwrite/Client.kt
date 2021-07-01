90240313a58663875e672c5643b610642b684696
Christy Jacob <christyjacob4@gmail.com>
2021-07-01 12:15:04 +0530

    feat: update the spec

--- library/src/main/java/io/appwrite/Client.kt
+++ library/src/main/java/io/appwrite/Client.kt
@@ -71,7 +71,7 @@ class Client @JvmOverloads constructor(
             "origin" to "appwrite-android://${context.packageName}",
             "user-agent" to "${context.packageName}/${appVersion}, ${System.getProperty("http.agent")}",
             "x-sdk-version" to "appwrite:android:${BuildConfig.SDK_VERSION}",            
-            "x-appwrite-response-format" to "0.8.0"
+            "x-appwrite-response-format" to "0.9.0"
         )
         config = mutableMapOf()
         
