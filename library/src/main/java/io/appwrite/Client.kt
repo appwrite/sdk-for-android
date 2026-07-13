9fe9907f19c501305919d7e1c96888007c9b9844
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2026-07-13 12:09:53 +0530

    chore: update Android SDK to 25.3.0

--- library/src/main/java/io/appwrite/Client.kt
+++ library/src/main/java/io/appwrite/Client.kt
@@ -142,7 +142,7 @@ class Client @JvmOverloads constructor(
      */
     fun setBearer(value: String): Client {
         config["bearer"] = value
-        addHeader("authorization", value)
+        addHeader("authorization", "Bearer $value")
         return this
     }
 
